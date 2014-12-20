7b450c59ae14aedc4971848363a8bf314f36b2e7
jonschlinkert <jon.schlinkert@sellside.com>
2014-12-20 14:39:27 -0500

    relative dest function

---
+++ index.js
@@ -0,0 +1,48 @@
+/*!
+ * relative-dest <https://github.com/jonschlinkert/relative-dest>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var path = require('path');
+var relative = require('relative');
+
+module.exports = function(fp, dest, slash) {
+  if (typeof dest !== 'string') {
+    return fp;
+  }
+
+  if (typeof fp !== 'string') {
+    throw new TypeError('relative-dest expects at least one string.');
+  }
+
+  slash = arguments[arguments.length - 1];
+  var orig = fp;
+  var last = fp[fp.length - 1];
+
+  var res = relative(fp, dest);
+  var len = res.length;
+  res = len > 0 ? res : '.';
+
+  // add or remove trailing slash, based on the orig path
+  if (slash === true) {
+    if (last === '/') {
+      res += '/';
+    } else if (last !== '/' && res[len - 1] === '/') {
+      res.substring(0, len - 1);
+    }
+  }
+
+  if (path.normalize(orig) === path.normalize(res) || res === '.') {
+    if (last === '/') {
+      return './';
+    } else {
+      return '';
+    }
+  }
+  // we need to normalize since it's used in URLs
+  return res.replace(/[\\\/]/g, '/');
+};
